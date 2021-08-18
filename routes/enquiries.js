f5a1d721cb15b455de3d3f60af212ab4947e8eae
pptp <ash.patil@capco.com>
2021-08-18 17:07:34 +0100

    add mock enquiries

---
+++ routes/enquiries.js
@@ -0,0 +1,61 @@
+const express = require('express')
+const uuid = require('uuid')
+const router = express.Router()
+
+const enquiries = [
+    {
+        enquiryResponseId: uuid.v4(),
+        swiftReft: 'SR-123',
+        customerId: 'CN-123',
+        accountNumber: 'AN-123',
+        accountName: 'John Doe',
+        receiverBIC: 'RBIC-012',
+        senderBIC: 'SBIC-012',
+        generatedTime: new Date(),
+        sgsStatus: 'valid',
+        messageContent: 'this message is self destructive in 5 seconds',
+    },
+    {
+        enquiryResponseId: uuid.v4(),
+        swiftReft: 'SR-456',
+        customerId: 'CN-456',
+        accountNumber: 'AN-456',
+        accountName: 'Jane Doe',
+        receiverBIC: 'RBIC-456',
+        senderBIC: 'SBIC-456',
+        generatedTime: new Date(),
+        sgsStatus: 'valid',
+        messageContent: 'this message is self destructive in 5 seconds',
+    },
+    {
+        enquiryResponseId: uuid.v4(),
+        swiftReft: 'SR-666',
+        customerId: 'CN-666',
+        accountNumber: 'AN-666',
+        accountName: 'John Smith',
+        receiverBIC: 'RBIC-666',
+        senderBIC: 'SBIC-666',
+        generatedTime: new Date(),
+        sgsStatus: 'valid',
+        messageContent: 'this message is self destructive in 5 seconds',
+    },
+    {
+        enquiryResponseId: uuid.v4(),
+        swiftReft: 'SR-007',
+        customerId: 'CN-07',
+        accountNumber: 'AN-007',
+        accountName: 'Jane Smith',
+        receiverBIC: 'RBIC-007',
+        senderBIC: 'SBIC-007',
+        generatedTime: new Date(),
+        sgsStatus: 'valid',
+        messageContent: 'this message is self destructive in 5 seconds',
+    },
+]
+
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+        res.send(enquiries)
+})
+
+module.exports = router
